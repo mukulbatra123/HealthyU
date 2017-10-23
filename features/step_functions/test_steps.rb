871d26bfa5d42ec1b20568073e831a4474da680a
Steven Liounis <sliouni1@binghamton.edu>
2017-10-23 16:36:09 +0000

    first cucumber iter test

---
+++ features/step_functions/test_steps.rb
@@ -0,0 +1,23 @@
+Given(/^I have not opened the application$/) do
+  
+end
+
+When("I click on the link") do |link|
+  click_link(link)
+end
+
+Then /I should be on the homepage/ do
+  visit "/"
+end
+
+Given(/^I am on the homepage$/) do
+  visit "/"
+end
+
+When("I look on the page") do
+  
+end
+
+Then /I should see the title "HealthyU"/ do
+  page.should have_content("HealthyU")
+end
